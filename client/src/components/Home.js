c7f8384b9a5746db47ae00074bfcb9edcf9081fe
joey <joeywongsw@gmail.com>
2020-09-18 00:07:54 +0800

    got seller shops view component out

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+
 import Search from "./Search";
+import SellerShops from "./SellerShops";
 import Results from "./Results";
 
 function Home() {
+
     const [input, setInput] = useState("");
     const [query, setQuery] = useState("");
     const [hasSearched, setHasSearched] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [sellerId, setSellerId] = useState();
+
+    useEffect(() => {
+        checkLoggedIn();
+    }, []);
+
+    const checkLoggedIn = () => {
+        if (Cookies.get('random')) {
+            setLoggedIn(true);
+            setSellerId(Cookies.get('random'));
+        }
+    }
+
     let onChangeHandler = (e) => {
         setInput(e.target.value);
     }
@@ -17,11 +35,15 @@ function Home() {
     return (
         <div>
             <h1>You are HOME</h1>
+
             <Search
                 onChangeHandler={onChangeHandler}
                 inputHandler={inputHandler}
                 input={input}
             />
+
+            {loggedIn ? <SellerShops sellerId={sellerId} /> : null}
+
             {hasSearched ? <Results query={query} /> : null}
         </div>
     )
--- models/shops.js
+++ models/shops.js
@@ -2,13 +2,28 @@ module.exports = (dbPoolInstance) => {
 
     let findShop = async (value) => {
         console.log(value, "models triggered")
-        let query='SELECT * FROM shops WHERE id=$1;'
+        let query = 'SELECT * FROM shops WHERE id=$1;'
         const result = await dbPoolInstance.query(query, value)
-        return(result)
+        return (result)
     }
-    
-    
-        return {
-            findShop
-        }
+
+    let getSellerShops = (sellerID, callback) => {
+
+        let query = `SELECT shops.id, shops.shop_name, shops.image_url FROM sellers INNER JOIN shops ON sellers.id = shops.seller_id WHERE sellers.id = ${sellerID}`;
+
+        dbPoolInstance.query(query, (err, result) => {
+            if (err) {
+                console.log("error at shops model, getSellerShops ---", err.message);
+                callback(null, null);
+            }
+            else {
+                callback(null, result);
+            }
+        })
     }
+
+    return {
+        findShop,
+        getSellerShops
+    }
+}
