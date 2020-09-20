1c1a10cbf915e71d649a56e7bb2a9ab1a5a96856
chewjta <chewjta@gmail.com>
2020-09-21 01:05:37 +0800

    css done

--- client/src/components/AllShops.js
+++ client/src/components/AllShops.js
@@ -18,7 +18,7 @@ function AllShops() {
     let shops = allShops.map((item, index) => {
         return (
                 <div class="card mb-4 col-3 mr-5 ml-5 d-inline-flex align-items-center" style={{display:'inline-block',maxHeight:'600px'}}>
-              <div style={{height:'400px'}} class="d-flex"><img class="card-img-top" src={item.image_url} alt="Card image cap" style={{objectFit:'fill', margin:'auto 0',alignSelf:'center'}}/></div>
+              <div style={{height:'400px'}} class="d-flex"><img class="card-img-top" src={item.image_url} alt="Card image cap" style={{objectFit:'contain', margin:'auto 0',alignSelf:'center'}}/></div>
               <div class="card-body mx-auto text-center">
                 <h5 class="card-title">{item.shop_name}</h5>
                 <p class="card-text"><small class="text-muted">{item.about}</small></p>
